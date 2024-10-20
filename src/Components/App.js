6d15c4c8addd27f84889316351d225ff2c9b7bc8
Eduardo Rita <rito_2815@hotmail.com>
2024-10-20 12:11:40 -0600

    feature: main component app

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,26 @@
-function App() {
+import { useState } from "react"
+import FilteredList from "./FilteredList"
+import SearchBox from "./SearchBox"
+import Equipos from "./Equipos"
 
-    
+function App() {
+    const [searchQuery, setSearchQuery] = useState('')
+    const [equiposData, setEquiposData] = useState(Equipos);
+
+    const handleSearchChange = (newQuery) => {
+        setSearchQuery(newQuery);
+        const filteredEquipos = Equipos.filter((equipo) => {
+            return equipo.name.toLowerCase().includes(newQuery.toLowerCase()) 
+        });
+        setEquiposData(filteredEquipos)
+    }
 
     return (
         <>
             <h1>Inicio de actividad</h1>
             <p>Listas de equipos</p>
+            <SearchBox onSearchChange={handleSearchChange} />
+            <FilteredList equipos={equiposData} searchQuery={searchQuery}/>
         </>
     )
 }
