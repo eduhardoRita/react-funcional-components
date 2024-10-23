657567fb844381f6bb049080e8d1ff6b28f64a2b
Eduardo Rita <rito_2815@hotmail.com>
2024-10-22 19:46:57 -0600

    feature: improvement in code oraganization and syntax

--- src/Components/Filter/FilteredList.js
+++ src/Components/Filter/FilteredList.js
@@ -1,46 +1,57 @@
 import React from "react";
+import "./styles.css"
 
 function FilteredList({equipos, searchQuery}) {
 
     console.log(equipos);
 
     return (
-        
-        <ul>
-            <h2>lol</h2>
-            {equipos.map((equipo) => (
-                <li key={equipo.name}>
-                    <h2>{equipo.name}</h2>
-                    <p>{equipo.contry}</p>
-                    <p>Entrenador: {equipo.coach}</p>
-                    <p>numero de jugadores: {equipo.memberNumber}</p>
-                    <h3>Porteros:</h3>
-                    <ul>
-                        {equipo.goalkeepers.map((portero) => (
-                            <li key={portero}>{portero}</li>
-                        ))}
-                    </ul>
-                    <h3>Defensas:</h3>
-                    <ul>
-                        {equipo.defenses.map((defensa) => (
-                            <li key={defensa}>{defensa}</li>
-                        ))}
-                    </ul>
-                    <h3>Centrocsmpistas:</h3>
-                    <ul>
-                        {equipo.midfielders.map((mediocampista) => (
-                            <li key={mediocampista}>{mediocampista}</li>
-                        ))}
-                    </ul>
-                    <h3>Delanteros:</h3>
-                    <ul>
-                        {equipo.fronts.map((delantero) => (
-                            <li key={delantero}>{delantero}</li>
-                        ))}
-                    </ul>
-                </li>
-            ))}
-        </ul>
+        <section className="container-filtered-list">
+            <ul className="lists-team">
+                {equipos.map((equipo) => (
+                    <li className="list-team" key={equipo.name}>
+                        <div className="main-div">
+                            <h3 className="name-team">{equipo.name}</h3>
+                            <p><b>Pais:</b> {equipo.contry}</p>
+                            <p><b>Entrenador:</b> {equipo.coach}</p>
+                            <p><b>Numero de jugadores:</b> {equipo.memberNumber}</p>
+                        </div>
+                        <div className="goalkeepers-div">
+                            <h4 className="subtitle-div">Porteros:</h4>
+                            <ul>
+                                {equipo.goalkeepers.map((portero) => (
+                                    <li key={portero}>{portero}</li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div className="defenses-div">
+                            <h4 className="subtitle-two-div">Defensas:</h4>
+                            <ul>
+                                {equipo.defenses.map((defensa) => (
+                                    <li key={defensa}>{defensa}</li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div className="midfielders-div">
+                            <h4 className="subtitle-three-div">Centrocsmpistas:</h4>
+                            <ul>
+                                {equipo.midfielders.map((mediocampista) => (
+                                    <li key={mediocampista}>{mediocampista}</li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div className="fronts-div">
+                            <h4 className="subtitle-four-div">Delanteros:</h4>
+                            <ul>
+                                {equipo.fronts.map((delantero) => (
+                                    <li key={delantero}>{delantero}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </section>             
     )
 }
 
